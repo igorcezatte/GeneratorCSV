e0e19e4b006f94a409a649b5fd7778ef28f4e3e1
Igor Cezatte <igorcezatte13@gmail.com>
2019-10-31 18:19:55 -0300

    fix: list of select on VSC page

--- src/Pages/VSC/index.js
+++ src/Pages/VSC/index.js
@@ -1,76 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { MdClear, MdDelete } from "react-icons/md";
-import CsvDownloader from 'react-csv-downloader';
-import { toast } from 'react-toastify';
-import { Form, Table, DownloadButton } from './styles';
-import { useFormik } from 'formik';
-import perfil1 from '../../tmp/lista';
+import CsvDownloader from "react-csv-downloader";
+import { toast } from "react-toastify";
+import { Form, Table, DownloadButton } from "./styles";
+import { useFormik } from "formik";
 
-import PageHeader from '../../components/PageHeader';
+import PageHeader from "../../components/PageHeader";
 
-import PageContainer from '../../components/PageContainer';
-import Container from '../../components/Container';
-import 'react-toastify/dist/ReactToastify.css';
+import PageContainer from "../../components/PageContainer";
+import Container from "../../components/Container";
+import "react-toastify/dist/ReactToastify.css";
 
-import logo from '../../assets/images/logo.png';
+import logo from "../../assets/images/logo.png";
 
-import list from '../../tmp/list';
+import list from "../../tmp/list";
 
 function VSC() {
   const [data, setData] = useState([]);
-  
-  const handleDelete = (index) => {
-    const newDt = data.filter((_, idx) => idx !== index)
-    setData(newDt)
-  }
+
+  const handleDelete = index => {
+    const newDt = data.filter((_, idx) => idx !== index);
+    setData(newDt);
+  };
 
   const formik = useFormik({
     initialValues: {
-      userId:'',
-      company: '',
-      supOrg:'',
-      supGroup: ''
+      userId: "",
+      company: "",
+      supOrg: "",
+      supGroup: ""
     },
-    
+
     onSubmit: values => {
-      
-      if(data.length >= 15) {
-        toast.error('Só é possível adicionar 15 acessos por planilha');
-        return
+      if (data.length >= 15) {
+        toast.error("Só é possível adicionar 15 acessos por planilha");
+        return;
       }
 
-      let nId = '', nCompany = '', nSupOrg = '', nSupGroup ='';
-    
-      if(!formik.values.userId){
-        nId =  <p>• ID do usuário</p>;
-      };
-      if(!formik.values.company){
+      let nId = "",
+        nCompany = "",
+        nSupOrg = "",
+        nSupGroup = "";
+
+      if (!formik.values.userId) {
+        nId = <p>• ID do usuário</p>;
+      }
+      if (!formik.values.company) {
         nCompany = <p>• Company</p>;
-      };
-      if(!formik.values.supOrg){
+      }
+      if (!formik.values.supOrg) {
         nSupOrg = <p>• Support Organization</p>;
-      };
-      if(!formik.values.supGroup){
+      }
+      if (!formik.values.supGroup) {
         nSupGroup = <p>• Support Group</p>;
-      };
-      if (!formik.values.userId || !formik.values.company || !formik.values.supOrg || !formik.values.supGroup) { 
-        toast.error(<div>Preencha: {nId}{nCompany}{nSupOrg}{nSupGroup}</div>);
-        return
-      };
-      
-      setData([values,...data]);
-      console.log(formik)
+      }
+      if (
+        !formik.values.userId ||
+        !formik.values.company ||
+        !formik.values.supOrg ||
+        !formik.values.supGroup
+      ) {
+        toast.error(
+          <div>
+            Preencha: {nId}
+            {nCompany}
+            {nSupOrg}
+            {nSupGroup}
+          </div>
+        );
+        return;
+      }
+
+      setData([values, ...data]);
+      console.log(formik);
     }
-  })
+  });
 
   return (
     <>
       <PageHeader title={"VSC Tool Services"} />
       <PageContainer>
         <Container>
-          <img src={logo} alt="logo-vale" ></img>
+          <img src={logo} alt="logo-vale"></img>
           <Form onSubmit={formik.handleSubmit}>
-            <label htmlFor='userId'>User ID</label>
+            <label htmlFor="userId">User ID</label>
             <input
               id="userId"
               name="userId"
@@ -79,48 +92,53 @@ function VSC() {
               value={formik.values.userId}
             />
 
-            <label htmlFor='company'>Company</label>
-            <select 
+            <label htmlFor="company">Company</label>
+            <select
               id="company"
-              name='company'
+              name="company"
               type="text"
               onChange={formik.handleChange}
               value={formik.values.company}
-              >
-                {perfil1.map(item => (
-                  <option key={item}>{item}</option>
-                ))}
-              </select>
-
-            <label htmlFor='supOrg'>Support Organization</label>
-            <select 
-            id='supOrg'
-            name='supOrg'
-            type='text'
-            onChange={formik.handleChange}
-            value={formik.values.supOrg}
             >
               {list.map(item => (
-                  <option key={item}>{item}</option>
-                ))}
+                <option key={item}>{item}</option>
+              ))}
             </select>
 
-            <label htmlFor='supGroup'>Support Group</label>
-            <select 
-              id='supGroup'
-              name='supGroup'
-              type='text'
+            <label htmlFor="supOrg">Support Organization</label>
+            <select
+              id="supOrg"
+              name="supOrg"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.supOrg}
+            >
+              {list.map(item => (
+                <option key={item}>{item}</option>
+              ))}
+            </select>
+
+            <label htmlFor="supGroup">Support Group</label>
+            <select
+              id="supGroup"
+              name="supGroup"
+              type="text"
               onChange={formik.handleChange}
               value={formik.values.supGroup}
             >
               {list.map(item => (
-                  <option key={item}>{item}</option>
-                ))}
+                <option key={item}>{item}</option>
+              ))}
             </select>
 
             <button type="submit">Add</button>
             <DownloadButton>
-              <CsvDownloader filename="multiple_requests" separator=";" noHeader={true} datas={data}>
+              <CsvDownloader
+                filename="multiple_requests"
+                separator=";"
+                noHeader={true}
+                datas={data}
+              >
                 Download CSV
               </CsvDownloader>
             </DownloadButton>
@@ -134,7 +152,9 @@ function VSC() {
                 <th>Company</th>
                 <th>Sup Org.</th>
                 <th>Sup Group</th>
-                <th><MdDelete size={20}/></th>
+                <th>
+                  <MdDelete size={20} />
+                </th>
               </tr>
               {data.map((d, idx) => (
                 <tr key={idx}>
@@ -144,7 +164,7 @@ function VSC() {
                   <td>{d.supGroup}</td>
                   <td>
                     <button onClick={() => handleDelete(idx)}>
-                      <MdClear size={20} color='red'/>
+                      <MdClear size={20} color="red" />
                     </button>
                   </td>
                 </tr>
